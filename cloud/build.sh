f1864ee4b166362d3382fd21be826e5eabd4c279
bjliuqiang <liuqianginfo@jd.com>
2020-10-26 20:27:23 +0800

    update image version

--- cloud/build.sh
+++ cloud/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cp -r ../build/bin compile/
 cp -r ../build/lib compile/
-docker build -t vearch/vearch:3.2.0 .
+docker build -t vearch/vearch:3.2.2 .
 rm -rf compile/bin
 rm -rf compile/lib
