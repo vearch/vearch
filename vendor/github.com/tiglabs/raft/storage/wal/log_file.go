bde880665e748a289513184829ac4f9b9714034c
lijie3 <bjlijie@jd.com>
2020-12-15 17:45:19 +0800

    fix the bug that the partition server can't restart successfully caused by raft log lost(such as machine suddenly down).

--- vendor/github.com/tiglabs/raft/storage/wal/log_file.go
+++ vendor/github.com/tiglabs/raft/storage/wal/log_file.go
@@ -148,15 +148,18 @@ func (lf *logEntryFile) ReBuildIndex() (truncateOffset int64, err error) {
 	}
 	filesize := info.Size()
 
-	var rec record
-	var offset int64
+    var (
+		rec              record
+		offset           int64
+		nextRecordOffset int64
+	)
 	r := newRecordReader(lf.f)
 	for {
 		offset, rec, err = r.Read()
 		if err != nil {
 			break
 		}
-
+		nextRecordOffset = r.offset
 		// log entry 更新索引
 		if rec.recType == recTypeLogEntry {
 			ent := &proto.Entry{}
@@ -179,8 +182,33 @@ func (lf *logEntryFile) ReBuildIndex() (truncateOffset int64, err error) {
 	if err == io.EOF {
 		err = nil
 	}
-	return filesize, err
+
+	var fixentry = func() (err error) {
+		if err = lf.openwrite(); err != nil {
+			return
+		}
+		if lf.index.len() == 0 {
+			err = lf.w.truncate(0)
+			return
+		}
+		var lastindexitem indexitem
+		if lastindexitem, err = lf.index.get(lf.index.last()); err == nil {
+			err = lf.w.truncate(int64(lastindexitem.offset))
+		}
+		return
+	}
+
+	if iserrcorrupt(err) || err == io.errunexpectedeof {
+		err = fixentry()
+	}
+	
+	if filesize != nextRecordOffset {
+		log.Warn("logName[%v],fileSize[%v],corrupt data after offset[%v]", lf.name, filesize, nextRecordOffset)
+	}
+	return offset, err
 }
+
+
 
 func (lf *logEntryFile) Name() logFileName {
 	return lf.name
