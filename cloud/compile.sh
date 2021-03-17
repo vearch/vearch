b67b7751784ff19471dbd34e677992cf2d2324d4
wxd <vip.da@qq.com>
2021-03-17 20:31:53 +0800

    vearch_env 3.2.6 (#385)
    * 1. update gamma: put faiss 1.6.4 into third_party, user no longer have to compile faiss
    2. update compile docs, remove faiss install
    
    * remove faiss install
    
    * vearch/vearch_env:3.2.5
    
    * vearch_env 3.2.6

--- cloud/compile.sh
+++ cloud/compile.sh
@@ -4,4 +4,4 @@
 git submodule init
 git submodule update --remote
 
-docker run -it -v $(dirname "$PWD"):/vearch vearch/vearch_env:3.2.5 /vearch/cloud/compile/compile.sh
+docker run -it -v $(dirname "$PWD"):/vearch vearch/vearch_env:3.2.6 /vearch/cloud/compile/compile.sh
