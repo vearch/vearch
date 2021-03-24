192177f01ab42a53bb5ddc112f2b206d01ac1730
bjliuqiang <liuqianginfo@jd.com>
2021-03-24 10:58:39 +0800

    update compile image

--- cloud/compile.sh
+++ cloud/compile.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
+#add env
+export GOROOT=/env/app/go
+export PATH=$PATH:$GOROOT/bin
+export ZFP_HOME=/env/app/zfp_install
+export ROCKSDB_HOME=/env/app/rocksdb_install
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROCKSDB_HOME/lib:$ZFP_HOME/lib
+# to compile
+cd /vearch/build
+mkdir -p /env/app/go/src/github.com/vearch
+ln -s /vearch/ /env/app/go/src/github.com/vearch
 
-# will download gamma in /engine with correct version
-git submodule init
-git submodule update
+cd /env/app/go/src/github.com/vearch/vearch/build
+./build.sh
 
-docker run -v $(dirname "$PWD"):/vearch vearch/vearch_env:3.2.5 /vearch/cloud/compile/compile.sh
+mkdir -p /vearch/build/lib/
+
+cp /env/app/rocksdb_install/lib/librocksdb.* /vearch/build/lib/
+cp /env/app/zfp_install/lib/libzfp.* /vearch/build/lib/
+cp /vearch/build/gamma_build/libgamma.* /vearch/build/lib/
+
+rm -rf /vearch/build/gamma_build
