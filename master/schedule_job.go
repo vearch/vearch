c6e9934a5ca1037fa6b3b78c3721ecb8b1924b18
gDreamcatcher <824792611@qq.com>
2021-11-01 17:19:41 +0800

    fix: memory and goroutine leak when doing cleanTask (#551)

--- master/schedule_job.go
+++ master/schedule_job.go
@@ -17,7 +17,7 @@ package master
 import (
 	"context"
 	"encoding/binary"
-	"fmt"
+	"errors"
 	"time"
 
 	"github.com/jasonlvhit/gocron"
@@ -30,79 +30,76 @@ import (
 
 const CronInterval = 60
 
-var spaceChannel = make(chan *entity.Space)
-
 func walkPartitions(masterServer *Server, partitions []*entity.Partition) {
 	ctx := masterServer.ctx
-	log.Info("Start Walking Partitions!")
+	log.Debug("Start Walking Partitions!")
 	for _, partition := range partitions {
 		if space, err := masterServer.client.Master().QuerySpaceByID(ctx, partition.DBId, partition.SpaceId); err != nil {
 			if vearchpb.NewError(vearchpb.ErrorEnum_INTERNAL_ERROR, err).GetError().Code == vearchpb.ErrorEnum_SPACE_NOTEXISTS {
-				log.Info("Could not find Space contains partition,PartitionID:[%d] so remove it from etcd!", partition.Id)
+				log.Warnf("Could not find Space contains partition,PartitionID:[%d] so remove it from etcd!", partition.Id)
 				partitionKey := entity.PartitionKey(partition.Id)
 				if err := masterServer.client.Master().Delete(ctx, partitionKey); err != nil {
-					log.Error("error:%s", err.Error())
+					log.Warnf("error:%s", err.Error())
 				}
 			} else {
-				log.Error("Failed to find space according dbid:[%d] spaceid:[%d] partitionID:[%d] err:[%s]", partition.DBId, partition.SpaceId, partition.Id, err.Error())
+				log.Warnf("Failed to find space according dbid:[%d] spaceid:[%d] partitionID:[%d] err:[%s]", partition.DBId, partition.SpaceId, partition.Id, err.Error())
 			}
 		} else {
 			if space == nil {
-				log.Info("Could not find Space contains partition,PartitionID:[%d] so remove it from etcd!", partition.Id)
+				log.Warnf("Could not find Space contains partition,PartitionID:[%d] so remove it from etcd!", partition.Id)
 				partitionKey := entity.PartitionKey(partition.Id)
 				if err := masterServer.client.Master().Delete(ctx, partitionKey); err != nil {
-					log.Error(err.Error())
+					log.Warnf(err.Error())
 				}
 			}
 		}
 	}
-	log.Info("Complete Walking Partitions!")
+	log.Debug("Complete Walking Partitions!")
 }
 
 func walkSpaces(masterServer *Server, spaces []*entity.Space) {
 	ctx := masterServer.ctx
-	log.Info("Start Walking Spaces!")
+	log.Debug("Start Walking Spaces!")
 	for _, space := range spaces {
-		spaceChannel <- space
 		if db, err := masterServer.client.Master().Get(ctx, entity.DBKeyBody(space.DBId)); err != nil {
-			log.Error("Failed to find database contains space,SpaceName:", space.Name, " SpaceID:", space.Id, " err:", err)
+			log.Warnf("Failed to get key[%s] from etcd, err: [%s]", entity.DBKeyBody(space.DBId), err.Error())
 		} else if db == nil {
-			log.Info("Could not find database contains space,,SpaceName:", space.Name, " SpaceID:", space.Id, " so remove it!")
+			log.Warnf("Could not find database contains space, SpaceName: %s, SpaceID: %s, so remove it!", space.Name, space.Id)
 			spaceKey := entity.SpaceKey(space.DBId, space.Id)
 			if err := masterServer.client.Master().Delete(ctx, spaceKey); err != nil {
-				log.Error("error:%s", err.Error())
+				log.Warnf("error: %s", err.Error())
 			}
 		}
 	}
-	log.Info("Complete Walking Spaces!")
+	log.Debug("Complete Walking Spaces!")
 }
 
 func removePartition(masterServer *Server, partitionServerRpcAddr string, pid entity.PartitionID) error {
-	log.Debug("Removing partition:[%s] from ps:[%s]", pid, partitionServerRpcAddr)
+	log.Debugf("Removing partition:[%s] from ps:[%s]", pid, partitionServerRpcAddr)
 	return client.DeletePartition(partitionServerRpcAddr, pid)
 }
 
 func walkServers(masterServer *Server, servers []*entity.Server) {
 	ctx := masterServer.ctx
-	log.Info("Start Walking Servers!")
+	log.Debug("Start Walking Servers!")
 	for _, server := range servers {
 		for _, pid := range server.PartitionIds {
 			if _, err := masterServer.client.Master().QueryPartition(ctx, pid); err != nil {
 				if vearchpb.NewError(vearchpb.ErrorEnum_INTERNAL_ERROR, err).GetError().Code == vearchpb.ErrorEnum_PARTITION_NOT_EXIST {
-					log.Info("to remove partition:%d", pid)
+					log.Warnf("to remove partition:%d", pid)
 					if err := removePartition(masterServer, server.RpcAddr(), pid); err != nil {
-						log.Warn("Failed to remove partition:%v allocated on server:%v,and err is:%v", pid, server.ID, err)
+						log.Warnf("Failed to remove partition: %v allocated on server: %v, and err is:%v", pid, server.ID, err)
 					}
 				} else {
-					log.Warn("Failed to find partition:", pid, " allocated on server:", server.ID, " err:", err)
+					log.Warnf("Failed to find partition: %v, allocated on server: %v, err: %v", pid, server.ID, err)
 				}
 			}
 		}
 	}
-	log.Info("Complete Walking Servers!")
+	log.Debug("Complete Walking Servers!")
 }
 
-var skipJob = fmt.Errorf("skip job")
+var errSkipJob = errors.New("skip job")
 
 func cleanTask(masterServer *Server) {
 
@@ -120,35 +117,35 @@ func cleanTask(masterServer *Server) {
 			stm.Put(entity.ClusterCleanJobKey, string(bytes))
 			return nil
 		}
-		return skipJob
+		return errSkipJob
 	})
-	if err == skipJob {
-		log.Info("skip clean task .....")
+	if err == errSkipJob {
+		log.Debug("skip clean task .....")
 		return
 	}
 	if err != nil {
-		log.Error("clean task has err for get ClusterCleanJobKey err: %s", err.Error())
+		log.Errorf("clean task has err for get ClusterCleanJobKey err: %s", err.Error())
 		return
 	}
 
 	log.Debug("Start clean task")
 	//process partitions
 	if partitions, err := masterServer.client.Master().QueryPartitions(masterServer.ctx); err != nil {
-		log.Error("Failed to get all partitions,err =", err.Error())
+		log.Errorf("Failed to get all partitions,err: %s", err.Error())
 	} else {
 		walkPartitions(masterServer, partitions)
 	}
 
 	//process spaces
 	if spaces, err := masterServer.client.Master().QuerySpacesByKey(masterServer.ctx, entity.PrefixSpace); err != nil {
-		log.Error("Failed to get all spaces,err =", err.Error())
+		log.Errorf("Failed to get all spaces,err: %s", err.Error())
 	} else {
 		walkSpaces(masterServer, spaces)
 	}
 
 	//process servers
 	if servers, err := masterServer.client.Master().QueryServers(masterServer.ctx); err != nil {
-		log.Error("Failed to get all servers,err = ", err.Error())
+		log.Errorf("Failed to get all servers,err: %s", err.Error())
 	} else {
 		walkServers(masterServer, servers)
 	}
