30e844ec11ef462f0bd6918853abab33f44d291b
Xiong LIU <liuxionghust@gmail.com>
2021-11-06 17:12:19 +0800

    search replica using round robin instead of random strategy

---
+++ client/round_robin.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"github.com/vearch/vearch/proto/entity"
+	"github.com/vearch/vearch/util/atomic"
+	"sync"
+)
+
+type ReplicaRoundRobin struct {
+	// key: partition id, value: atomic counter
+	counterMap sync.Map
+}
+
+func NewReplicaRoundRobin() *ReplicaRoundRobin {
+	return &ReplicaRoundRobin{}
+}
+
+func (rr *ReplicaRoundRobin) Next(pid entity.PartitionID, replicas []entity.NodeID) entity.NodeID {
+	if len(replicas) <= 0 {
+		return 0
+	}
+
+	var ix uint64
+	counter, ok := rr.counterMap.LoadOrStore(pid, atomic.NewCounter(1))
+	if ok {
+		// loaded
+		newValue := counter.(*atomic.AtomicCounter).Incr()
+		ix = (newValue - 1) % uint64(len(replicas))
+	}
+	return replicas[ix]
+}
