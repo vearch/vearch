f6c664538034a208b4386a2c0476bed94b460010
wangfuqiang <wangfuqiangg@126.com>
2023-03-29 20:24:56 +0800

    添加 cubefs mount 脚本

--- tools/deployment/demo/start.sh
+++ tools/deployment/demo/start.sh
@@ -2,6 +2,31 @@
 BasePath=$(cd `dirname $0`; pwd)
 cd $BasePath
 
+# mount a local path to cubefs, The cubefs cluster must already exists, this feature is default close
+MOUNT_CEBE=0
+MOUNT_POINT=XXXXXXX
+VOL_NAME=XXXXXXX
+OWNER=XXXXXXX
+AK=XXXXXXX
+SK=XXXXXXX
+MASTER_ADDR=XXXXXXX
+LOG_DIR=XXXXXXX
+MOUNT_SCRIPT_URL=XXXXXXX
+
+function mount {
+    if [[ ${MOUNT_CEBE} -eq 0 ]];then
+       echo "cubefs feature is close, will start vearch with local disk path"
+       return
+    fi
+    echo "will mount local path ${MOUNT_POINT} to cubefs ..."
+    curl ${MOUNT_SCRIPT_URL} |bash -s  ${MOUNT_POINT} ${VOL_NAME} ${OWNER} ${AK} ${SK} ${MASTER_ADDR} ${LOG_DIR}
+    # check if the cubefs client works 
+    ps -ef | grep cfs-client | grep -v grep
+    if [[ ${ret} -ne 0 ]];then
+        echo "cubefs mount fail, will start vearch with local disk path"
+    fi
+}
+
 function getServiceStatusInfo {
     pidFile=$1
     filterTag=$2
@@ -34,6 +59,7 @@ if [ -z "$1" ]; then
 fi
 
 if [ -n "$1" ]; then 
+    mount
     start $1 
 fi
 
