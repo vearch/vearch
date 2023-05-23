1d43b8c61474f8cff4a1ec635af32d2470d6b816
wangxingda <wangxingda@jd.com>
2023-05-23 22:30:40 +0800

    upgrade vearch/vearch:3.2.7 to vearch/vearch:latest

--- cloud/build.sh
+++ cloud/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cp -r ../build/bin compile/
 cp -r ../build/lib compile/
-docker build -t vearch/vearch:3.2.7 .
+docker build -t vearch/vearch:latest .
 rm -rf compile/bin
 rm -rf compile/lib
