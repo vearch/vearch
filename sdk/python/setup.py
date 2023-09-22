e5e335613cfeb99c33adfb37c72bb977ce51cf0b
zcdb <zhanchao0506@outlook.com>
2023-09-22 09:47:36 +0800

    add python sdk for vearch cluster

---
+++ sdk/python/setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup
+
+
+setup(
+    name='vearch_cluster',
+    version='0.0.1',
+    description='A library for efficient similarity search and storage of deep learning vectors.',
+    url='https://github.com/vearch/vearch',
+    author='vearch author',
+    author_email='vearch-maintainers@groups.io',
+    license='Apache License, Version 2.0',
+    keywords='real time index, vector nearest neighbors'
+)
