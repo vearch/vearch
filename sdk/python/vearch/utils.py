74ebbe2919c3ae4ca27e5c159dbe240baed68f62
yunge5 <1510938176@qq.com>
2024-03-05 18:23:25 +0800

    add name valid check logic

--- sdk/python/vearch/utils.py
+++ sdk/python/vearch/utils.py
@@ -1,4 +1,5 @@
 from enum import IntEnum
+import re
 
 
 def singleton(cls):
@@ -23,7 +24,7 @@ class DataType:
 
 
 class MetricType:
-    inner_product = "InnerProduct"
+    Inner_product = "InnerProduct"
     L2 = "L2"
 
 
@@ -35,14 +36,29 @@ index type
 """
 
 
-class IndexType:
-    SCALAR = 0
-    IVFPQ = 1
-    IVFFLAT = 2
-    BINARYIVF = 3
-    FLAT = 4
-    HNSW = 5
-    GPU_IVFPQ = 6
-    SSG = 7
-    IVFPQ_RELAYOUT = 8
-    SCANN = 9
+class IndexType(IntEnum):
+    NONE = 0,
+    SCALAR = 1,
+    IVFPQ = 2,
+    IVFFLAT = 3,
+    BINARYIVF = 4,
+    FLAT = 5,
+    HNSW = 6,
+    GPU_IVFPQ = 7,
+    SSG = 8,
+    IVFPQ_RELAYOUT = 9,
+    SCANN = 10,
+    UNKNOWN = 999
+
+
+reg_exp = "^([a-zA-Z]+)([a-z0-9A-Z]*[\-\_]{0,1}[a-z0-9A-Z]+)+"
+
+
+def name_valid_check(name: str) -> bool:
+    pattern = re.compile(reg_exp)
+    match_ret = pattern.match(name)
+    return match_ret.span()[1]-match_ret.span()[0] == len(name)
+
+
+if __name__ == "__main__":
+    print(name_valid_check("fdafjdsj_fdasjf"))
