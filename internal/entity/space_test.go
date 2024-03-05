15664e7475825dfdb6d6995286d9f4a20e9a9346
wu <klosewu@gmail.com>
2024-03-05 17:29:11 +0800

    add space test cases
    add space test cases

--- internal/entity/space_test.go
+++ internal/entity/space_test.go
@@ -22,12 +22,11 @@ import (
 	"github.com/vearch/vearch/internal/util/assert"
 )
 
-func TestSpaceString(t *testing.T) {
+func TestEngineErrorSpaceString(t *testing.T) {
 	space := &entity.Space{}
-	str := `{"engine": "caprice"}`
-	if err := json.Unmarshal([]byte(str), &space); err != nil {
-		t.Errorf(err.Error())
-	}
+	str := `{"name":"ts_space","partition_num":1,"replica_num":1,"engine":{"index_size":100000,"metric_type":"InnerProduct","retrieval_type":"ERROR_INDEX","retrieval_param":{"metric_type":"InnerProduct","ncentroids":128,"nsubvector":32,"nlinks":32,"efConstruction":200,"efSearch":64}},"properties":{"string":{"type":"string","array":true,"index":true},"float":{"type":"float","index":true},"int":{"type":"integer","index":true},"double":{"type":"double","index":true},"vector":{"type":"vector","dimension":128,"store_type":"MemoryOnly","format":"normalization"}}}`
+	err := json.Unmarshal([]byte(str), &space)
+	assert.NotNil(t, err)
 }
 
 func TestEngineSpaceString(t *testing.T) {
@@ -37,4 +36,86 @@ func TestEngineSpaceString(t *testing.T) {
 		t.Errorf(err.Error())
 	}
 	assert.Equal(t, space.Engine.IndexSize, int64(100000), "unmarshal string to engine err")
+}
+
+func TestSpace_Validate(t *testing.T) {
+	type fields struct {
+		Id              entity.SpaceID
+		Name            string
+		ResourceName    string
+		Version         entity.Version
+		DBId            entity.DBID
+		Enabled         *bool
+		Partitions      []*entity.Partition
+		PartitionNum    int
+		ReplicaNum      uint8
+		Properties      json.RawMessage
+		Engine          *entity.Engine
+		Models          json.RawMessage
+		SpaceProperties map[string]*entity.SpaceProperties
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "Valid space name",
+			fields: fields{
+				Name: "validSpaceName",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Empty space name",
+			fields: fields{
+				Name: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Space name starts with number",
+			fields: fields{
+				Name: "1invalidSpaceName",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Space name starts with underscore",
+			fields: fields{
+				Name: "_invalidSpaceName",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Space name with invalid characters",
+			fields: fields{
+				Name: "invalid!Space@Name",
+			},
+			wantErr: true,
+		},
+		// TODO: Add more test cases as needed.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			space := &entity.Space{
+				Id:              tt.fields.Id,
+				Name:            tt.fields.Name,
+				ResourceName:    tt.fields.ResourceName,
+				Version:         tt.fields.Version,
+				DBId:            tt.fields.DBId,
+				Enabled:         tt.fields.Enabled,
+				Partitions:      tt.fields.Partitions,
+				PartitionNum:    tt.fields.PartitionNum,
+				ReplicaNum:      tt.fields.ReplicaNum,
+				Properties:      tt.fields.Properties,
+				Engine:          tt.fields.Engine,
+				Models:          tt.fields.Models,
+				SpaceProperties: tt.fields.SpaceProperties,
+			}
+			if err := space.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("Space.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
